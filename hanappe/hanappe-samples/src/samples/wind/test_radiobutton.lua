835a0d3ce3655df88533b0e24c8c460c7271ce27
xiaoxiao-geng <gengxiaoxiao@3gwin.cn>
2013-09-16 23:21:36 +0800

    add weak ref sample code

--- hanappe/hanappe-samples/src/samples/wind/test_radiobutton.lua
+++ hanappe/hanappe-samples/src/samples/wind/test_radiobutton.lua
@@ -4,7 +4,7 @@ function onCreate( params )
 	view = View { scene = scene }
 
 	_G.pool = _G.pool or {}
-	setmetatable( pool ,{ __mode = "v" } )
+	setmetatable( pool ,{ __mode = "kv" } )
 
 	Button { parent = view, pos = { 100, 100 }, text = "click me", onClick = onClick }
 
@@ -20,11 +20,11 @@ function onClick()
 	MOAISim.forceGarbageCollection()
 	for k, v in pairs( pool ) do print( "  ", k, v ) end
 
-	local a = "1"
-	local b = "2"
+	local a = { 1, 2, 3 }
+	local b = { 3, 2, 1 }
 
-	pool[ "a" ] = a
-	pool[ "b" ] = b
+	table.insert( pool, a )
+	table.insert( pool, b )
 
 	print("  loop 1")
 	MOAISim.forceGarbageCollection()
